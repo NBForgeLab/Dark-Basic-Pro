01f98d365d8053be779981bc9c9239d4cabc9e44
NB2030 <batahf2030@hotmail.com>
2026-07-24 20:38:30 +0300

    Phase G TDD: Add C++17/20 attributes [[nodiscard]] and noexcept to ASTNode and modern C++ safety tests

--- DBProCompiler/DBPCompiler/ASTNode.h
+++ DBProCompiler/DBPCompiler/ASTNode.h
@@ -17,8 +17,8 @@ public:
     virtual ~ASTNode() = default;
     virtual void Accept(ASTVisitor* visitor) = 0;
 
-    const SourceLocation& GetLocation() const { return m_location; }
-    void SetLocation(const SourceLocation& loc) { m_location = loc; }
+    [[nodiscard]] const SourceLocation& GetLocation() const noexcept { return m_location; }
+    void SetLocation(const SourceLocation& loc) noexcept { m_location = loc; }
 
 protected:
     SourceLocation m_location;
